3120632863e97b6bc15be815de97f5918aaa83b4
SilviyaIvanova91 <99129696+SilviyaIvanova91@users.noreply.github.com>
2022-06-24 12:34:56 +0300

    added files from drawing with loops exercises

---
+++ JavaScript Basic/other/07. drawing with loop/christmasTree.js
@@ -0,0 +1,39 @@
+function christmasTree(input) {
+  let n = Number(input[0]);
+  let str = "";
+  for (let row = 0; row <= n; row++) {
+    str = "";
+    for (let i = 1; i <= n - row; i++) {
+      str += " ";
+    }
+    for (let j = 1; j <= row; j++) {
+      str += "" + "*";
+    }
+    str += " | ";
+    for (let k = 1; k <= row; k++) {
+      str += "" + "*";
+    }
+    for (let l = 1; l <= n - row; l++) {
+      str += " ";
+    }
+    console.log(str);
+  }
+}
+
+christmasTree(["1"]);
+christmasTree(["2"]);
+christmasTree(["3"]);
+christmasTree(["4"]);
+
+// function christmasTree(input) {
+//   let n = Number(input[0]);
+//   for (let row = 0; row <= n; row++) {
+//     console.log(
+//       " ".repeat(n - row) +
+//         "*".repeat(row) +
+//         " | " +
+//         "*".repeat(row) +
+//         " ".repeat(n - row)
+//     );
+//   }
+// }
---
+++ JavaScript Basic/other/07. drawing with loop/house.js
@@ -0,0 +1,57 @@
+function house(input) {
+  let n = Number(input[0]);
+  let starsCount = 0;
+  let count = 0;
+  let str = "";
+
+  for (let i = 0; i < Math.floor((n + 1) / 2); i++) {
+    if (n % 2 == 0) {
+      starsCount = 2 * (i + 1);
+      count = 2 * (i - 1);
+    } else {
+      starsCount = 2 * i + 1;
+      count = 2 * i - 1;
+    }
+    str = "";
+    for (let i = 1; i <= (n - starsCount) / 2; i++) {
+      str += "-";
+    }
+    for (let j = 1; j <= starsCount; j++) {
+      str += "*";
+    }
+    for (let k = 1; k <= (n - starsCount) / 2; k++) {
+      str += "-";
+    }
+    console.log(str);
+  }
+
+  str = "";
+  for (let i = 1; i <= n / 2; i++) {
+    str = "|";
+    for (let j = 0; j < Math.floor(n - 2); j++) {
+      str += "*";
+    }
+    str += "|";
+    console.log(str);
+  }
+}
+
+house(["2"]);
+house(["3"]);
+house(["4"]);
+house(["5"]);
+house(["6"]);
+
+// Draw the roof
+// for (let i = 0; i < Math.floor((n + 1) / 2); i++) {
+//   if (n % 2 == 0) starsCount = 2 * (i + 1);
+//   else starsCount = 2 * i + 1;
+//   console.log(
+//     "-".repeat((n - starsCount) / 2) +
+//       "*".repeat(starsCount) +
+//       "-".repeat((n - starsCount) / 2)
+//   );
+// }
+// for (let i = 0; i < Math.floor(n / 2); i++) {
+// Draw the house body: |*****| }
+// }
