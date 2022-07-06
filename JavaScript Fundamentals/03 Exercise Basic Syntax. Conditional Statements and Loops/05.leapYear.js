9af374e6e2ca2d62a9e4090c766bcdc4e100e7a5
SilviyaIvanova91 <99129696+SilviyaIvanova91@users.noreply.github.com>
2022-07-06 14:23:20 +0300

    added files from exercises

---
+++ JavaScript Fundamentals/03 Exercise Basic Syntax. Conditional Statements and Loops/05.leapYear.js
@@ -0,0 +1,19 @@
+function leapYear(number) {
+  if ((number % 4 === 0 && number % 100 !== 0) || number % 400 === 0) {
+    console.log(`yes`);
+  } else {
+    console.log(`no`);
+  }
+}
+
+leapYear(1984);
+leapYear(2003);
+leapYear(4);
+
+// function leapYear(number) {
+//   let result =
+//     (number % 4 === 0 && number % 100 !== 0) || number % 400 === 0
+//       ? "yes"
+//       : "no";
+//   console.log(result);
+// }
---
+++ JavaScript Fundamentals/03 Exercise Basic Syntax. Conditional Statements and Loops/07.triangleOfNumbers.js
@@ -0,0 +1,31 @@
+function triangleOfNumbers(num) {
+  let str = "";
+  for (let i = 1; i <= num; i++) {
+    for (let j = 1; j <= i; j++) {
+      str += i + " ";
+    }
+    console.log(str);
+    str = "";
+  }
+}
+
+triangleOfNumbers(3);
+triangleOfNumbers(5);
+triangleOfNumbers(6);
+
+// function triangleOfNumbers(num) {
+//   for (let i = 1; i <= num; i++) {
+//     let str = "";
+//     for (let j = 1; j <= i; j++) {
+//       str += i;
+//       if (j !== i) {
+//         str += " ";
+//       }
+//     }
+//     console.log(str);
+//   }
+// }
+
+// triangleOfNumbers(3);
+// triangleOfNumbers(5);
+// triangleOfNumbers(6);
