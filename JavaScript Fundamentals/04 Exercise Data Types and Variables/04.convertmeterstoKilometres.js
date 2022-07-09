7e54ec2796fddc66f587e966c5bab34de0b31609
SilviyaIvanova91 <99129696+SilviyaIvanova91@users.noreply.github.com>
2022-07-09 19:01:07 +0300

    added files from Data Types and Variables exercises

---
+++ JavaScript Fundamentals/04 Exercise Data Types and Variables/04.convertmeterstoKilometres.js
@@ -0,0 +1,7 @@
+function convertmeterstoKilometres(meters) {
+  let kilometers = meters / 1000;
+  console.log(kilometers.toFixed(2));
+}
+
+convertmeterstoKilometres(1852);
+convertmeterstoKilometres(798);
---
+++ JavaScript Fundamentals/04 Exercise Data Types and Variables/05.poundstoDollars.js
@@ -0,0 +1,8 @@
+function poundstoDollars(pound) {
+  let dolla = 1.31;
+  let all = pound * dolla;
+  console.log(all.toFixed(3));
+}
+
+poundstoDollars(80);
+poundstoDollars(39);
