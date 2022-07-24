ee3047f6b217b4fb2bd8eb235e26d87d0c9fa2c6
SilviyaIvanova91 <99129696+SilviyaIvanova91@users.noreply.github.com>
2022-07-24 12:22:40 +0300

    added files from Object and Classes exercise

---
+++ JavaScript Fundamentals/22 Exercise Objects and Classes/08.classStorage.js
@@ -0,0 +1,30 @@
+class Storage {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.storage = [];
+    this.totalCost = 0;
+  }
+
+  addProduct(product) {
+    this.capacity -= product.quantity;
+    this.totalCost += product.price * product.quantity;
+    this.storage.push(product);
+  }
+
+  getProducts() {
+    return this.storage.map((product) => JSON.stringify(product)).join("\n");
+  }
+}
+
+let productOne = { name: "Cucamber", price: 1.5, quantity: 15 };
+let productTwo = { name: "Tomato", price: 0.9, quantity: 25 };
+let productThree = { name: "Bread", price: 1.1, quantity: 8 };
+let storage = new Storage(50);
+
+storage.addProduct(productOne);
+storage.addProduct(productTwo);
+storage.addProduct(productThree);
+
+console.log(storage.getProducts());
+console.log(storage.capacity);
+console.log(storage.totalCost);
