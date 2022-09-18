2dc11ae642cb943c66ccf4ddaa5addb1d7a125f8
SilviyaIvanova91 <99129696+SilviyaIvanova91@users.noreply.github.com>
2022-09-18 11:15:42 +0300

    added exercises from Arrays and Nested Arrays

---
+++ JavaScript Advanced/05 Exercise Arrays and Nested Arrays/06.listOfNames.js
@@ -0,0 +1,15 @@
+function listOfNames(arr) {
+  arr.sort((a, b) => a.localeCompare(b));
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`${i + 1}.${arr[i]}`);
+  }
+}
+
+listOfNames(["John", "Bob", "Christina", "Ema"]);
+
+//-----------------------------------------
+// function listOfNames(arr) {
+//   arr
+//     .sort((a, b) => a.localeCompare(b))
+//     .forEach((el, i) => console.log(`${i + 1}.${el}`));
+// }
