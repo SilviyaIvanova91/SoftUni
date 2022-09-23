071417468931113828e6fe15295967e538030b4a
SilviyaIvanova <silviya.yordanova91@gmail.com>
2022-09-23 16:03:47 +0300

    Update 05.sumEvenNumbers.js

--- JavaScript Fundamentals/05 Arrays/05.sumEvenNumbers.js
+++ JavaScript Fundamentals/05 Arrays/05.sumEvenNumbers.js
@@ -14,15 +14,15 @@ sumEvenNumbers(["3", "5", "7", "9"]);
 sumEvenNumbers(["2", "4", "6", "8", "10"]);
 
 //---------------------------------------------
-// function sumEvenNumbers(arr) {
-//     for (let i = 0; i < arr.length; i++) {
-//       arr[i] = Number(arr[i]);
-//     }
-//     let sum = 0
-//     for (let num of arr) {
-//         if (num] % 2 == 0) {
-//             sum += num;
-//           }
-//     }
-//     console.log(sum);
-//   }
+ function sumEvenNumbers(arr) {
+     for (let i = 0; i < arr.length; i++) {
+       arr[i] = Number(arr[i]);
+     }
+     let sum = 0
+     for (let num of arr) {
+         if (num] % 2 == 0) {
+             sum += num;
+           }
+     }
+     console.log(sum);
+   }
