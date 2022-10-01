8a5acbb4351ad3a06a0d28f2bf153384d6ebbcb4
SilviyaIvanova <silviya.yordanova91@gmail.com>
2022-10-01 11:22:14 +0300

    Update a.js

--- JavaScript Basic/03 Nested Conditional Statements/lab/a.js
+++ JavaScript Basic/03 Nested Conditional Statements/lab/a.js
@@ -1,12 +1,14 @@
 function tradeComision(input) {
   let city = input[0];
   let sales = Number(input[1]);
-  // let isValid = 0 <= sales && sales <= 500;
-  // let isValid1 = 500 < sales && sales <= 1000;
-  // let isValid2 = 1000 < sales && sales <= 10000;
-  // let isValid3 = sales > 10000;
-  // let finalSum = 0;
-
+  
+  //-------------other case-------------
+  let isValid = 0 <= sales && sales <= 500;
+  let isValid1 = 500 < sales && sales <= 1000;
+  let isValid2 = 1000 < sales && sales <= 10000;
+  let isValid3 = sales > 10000;
+  let finalSum = 0;
+//-----------------------------------------
   if (0 <= trade && trade <= 500) {
     switch (city) {
       case "Sofia":
@@ -75,69 +77,71 @@ function tradeComision(input) {
     console.log((comission / 100).toFixed(2));
   }
 }
-//   if (isValid) {
-//     switch (city) {
-//       case "Sofia":
-//         finalSum = 0.05 * sales;
-//         break;
-//       case "Varna":
-//         finalSum = 0.045 * sales;
-//         break;
-//       case "Plovdiv":
-//         finalSum = 0.055 * sales;
-//         break;
-//       default:
-//         console.log("error");
-//     }
-//   } else if (isValid1) {
-//     switch (city) {
-//       case "Sofia":
-//         finalSum = 0.07 * sales;
-//         break;
-//       case "Varna":
-//         finalSum = 0.075 * sales;
-//         break;
-//       case "Plovdiv":
-//         finalSum = 0.08 * sales;
-//         break;
-//       default:
-//         console.log("error");
-//     }
-//   } else if (isValid2) {
-//     switch (city) {
-//       case "Sofia":
-//         finalSum = 0.08 * sales;
-//         break;
-//       case "Varna":
-//         finalSum = 0.1 * sales;
-//         break;
-//       case "Plovdiv":
-//         finalSum = 0.12 * sales;
-//         break;
-//       default:
-//         console.log("error");
-//     }
-//   } else if (isValid3) {
-//     switch (city) {
-//       case "Sofia":
-//         finalSum = 0.12 * sales;
-//         break;
-//       case "Varna":
-//         finalSum = 0.13 * sales;
-//         break;
-//       case "Plovdiv":
-//         finalSum = 0.145 * sales;
-//         break;
-//       default:
-//         console.log("error");
-//     }
-//   } else {
-//     console.log("error");
-//   }
-//   {
-//     console.log(finalSum.toFixed(2));
-//   }
-// }
+
+//---------------------------------------------------------------------------
+   if (isValid) {
+     switch (city) {
+       case "Sofia":
+         finalSum = 0.05 * sales;
+         break;
+       case "Varna":
+         finalSum = 0.045 * sales;
+         break;
+       case "Plovdiv":
+         finalSum = 0.055 * sales;
+         break;
+       default:
+         console.log("error");
+     }
+   } else if (isValid1) {
+     switch (city) {
+       case "Sofia":
+         finalSum = 0.07 * sales;
+         break;
+       case "Varna":
+         finalSum = 0.075 * sales;
+         break;
+       case "Plovdiv":
+         finalSum = 0.08 * sales;
+         break;
+       default:
+         console.log("error");
+     }
+   } else if (isValid2) {
+     switch (city) {
+       case "Sofia":
+         finalSum = 0.08 * sales;
+         break;
+       case "Varna":
+         finalSum = 0.1 * sales;
+         break;
+       case "Plovdiv":
+         finalSum = 0.12 * sales;
+         break;
+       default:
+         console.log("error");
+     }
+   } else if (isValid3) {
+     switch (city) {
+       case "Sofia":
+         finalSum = 0.12 * sales;
+         break;
+       case "Varna":
+         finalSum = 0.13 * sales;
+         break;
+       case "Plovdiv":
+         finalSum = 0.145 * sales;
+         break;
+       default:
+         console.log("error");
+     }
+   } else {
+     console.log("error");
+   }
+   {
+     console.log(finalSum.toFixed(2));
+   }
+ }
 
 tradeComision(["Sofia", "1500"]);
 tradeComision(["Plovdiv", "499.99"]);
