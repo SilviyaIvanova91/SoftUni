3f6bbaf655737515e8e6fe67992474419aa8ba3f
SilviyaIvanova <silviya.yordanova91@gmail.com>
2022-10-05 11:58:09 +0300

    Update highJump.js

--- JavaScript Basic/Old Exams/06. 9 and 10 March 2019/highJump.js
+++ JavaScript Basic/Old Exams/06. 9 and 10 March 2019/highJump.js
@@ -40,40 +40,40 @@ function highJump(input) {
 highJump(["231", "205", "212", "213", "228", "229", "230", "235"]);
 highJump(["250", "225", "224", "225", "228", "231", "235", "234", "235"]);
 
-// function highJump(input) {
-//     let index = 0;
-//     let target = Number(input[index++]);
-//     let oldstart = target - 30;
-//     let start = 0;
-//     let fails = 0;
-//     let everyJump = 0;
-//     let counterJump = 0;
-//     let command = input[index++];
-//     while (target >= everyJump) {
-//       let currentJump = Number(command);
-//       let start = oldstart;
-//       start += start;
-//       counterJump++;
+ function highJump(input) {
+     let index = 0;
+     let target = Number(input[index++]);
+     let oldstart = target - 30;
+     let start = 0;
+     let fails = 0;
+     let everyJump = 0;
+     let counterJump = 0;
+     let command = input[index++];
+     while (target >= everyJump) {
+       let currentJump = Number(command);
+       let start = oldstart;
+       start += start;
+       counterJump++;
 
-//       if (currentJump > start) {
-//         if (currentJump > target) {
-//           break;
-//         }
-//         start += 5;
-//         fails = 0;
-//       } else {
-//         fails++;
-//       }
-//       if (fails === 3) {
-//         break;
-//       }
-//       command = input[index++];
-//     }
-//     if (target <= everyJump) {
-//       console.log(
-//         `Tihomir succeeded, he jumped over ${start}cm after ${counterJump} jumps.`
-//       );
-//     } else {
-//       console.log(`Tihomir failed at ${start}cm after ${counterJump} jumps.`);
-//     }
-//   }
+       if (currentJump > start) {
+         if (currentJump > target) {
+           break;
+         }
+         start += 5;
+         fails = 0;
+       } else {
+         fails++;
+       }
+       if (fails === 3) {
+         break;
+       }
+       command = input[index++];
+     }
+     if (target <= everyJump) {
+       console.log(
+         `Tihomir succeeded, he jumped over ${start}cm after ${counterJump} jumps.`
+       );
+     } else {
+       console.log(`Tihomir failed at ${start}cm after ${counterJump} jumps.`);
+     }
+   }
