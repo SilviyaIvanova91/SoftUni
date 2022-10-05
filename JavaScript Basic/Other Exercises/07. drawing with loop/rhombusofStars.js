177af8532de9306849ef3dbe4b55031708cabc27
SilviyaIvanova <silviya.yordanova91@gmail.com>
2022-10-05 12:09:25 +0300

    Update rhombusofStars.js

--- JavaScript Basic/Other Exercises/07. drawing with loop/rhombusofStars.js
+++ JavaScript Basic/Other Exercises/07. drawing with loop/rhombusofStars.js
@@ -34,25 +34,25 @@ rhombusofStars(["2"]);
 rhombusofStars(["3"]);
 rhombusofStars(["4"]);
 
-// for (let row = 0; row < n - 1; row++) {
-//     console.log(
-//       " ".repeat(n - 1 - row) + "* ".repeat(row + 1) + " ".repeat(n - 1 - row)
-//     );
-//   }
-//   console.log("* ".repeat(n));
-//   for (let row = n - 2; row >= 0; row--) {
-//     console.log(
-//       " ".repeat(n - 1 - row) + "* ".repeat(row + 1) + " ".repeat(n - 1 - row)
-//     );
-//   }
-// for (let row = 0; row < n - 1; row++) {
-//     console.log(
-//       " ".repeat(n - 1 - row) + "* ".repeat(row + 1) + " ".repeat(n - 1 - row)
-//     );
-//   }
-//   console.log("* ".repeat(n));
-//   for (let row = n - 2; row >= 0; row--) {
-//     console.log(
-//       " ".repeat(n - 1 - row) + "* ".repeat(row + 1) + " ".repeat(n - 1 - row)
-//     );
-//   }
+ for (let row = 0; row < n - 1; row++) {
+     console.log(
+       " ".repeat(n - 1 - row) + "* ".repeat(row + 1) + " ".repeat(n - 1 - row)
+     );
+   }
+   console.log("* ".repeat(n));
+   for (let row = n - 2; row >= 0; row--) {
+     console.log(
+       " ".repeat(n - 1 - row) + "* ".repeat(row + 1) + " ".repeat(n - 1 - row)
+     );
+   }
+ for (let row = 0; row < n - 1; row++) {
+     console.log(
+       " ".repeat(n - 1 - row) + "* ".repeat(row + 1) + " ".repeat(n - 1 - row)
+     );
+   }
+   console.log("* ".repeat(n));
+   for (let row = n - 2; row >= 0; row--) {
+     console.log(
+       " ".repeat(n - 1 - row) + "* ".repeat(row + 1) + " ".repeat(n - 1 - row)
+     );
+   }
