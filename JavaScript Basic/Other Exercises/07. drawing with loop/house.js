f3ec9aa616f05c3be7784f59d766d4a5ddd37ae9
SilviyaIvanova <silviya.yordanova91@gmail.com>
2022-10-05 12:08:46 +0300

    Update house.js

--- JavaScript Basic/Other Exercises/07. drawing with loop/house.js
+++ JavaScript Basic/Other Exercises/07. drawing with loop/house.js
@@ -43,15 +43,15 @@ house(["5"]);
 house(["6"]);
 
 // Draw the roof
-// for (let i = 0; i < Math.floor((n + 1) / 2); i++) {
-//   if (n % 2 == 0) starsCount = 2 * (i + 1);
-//   else starsCount = 2 * i + 1;
-//   console.log(
-//     "-".repeat((n - starsCount) / 2) +
-//       "*".repeat(starsCount) +
-//       "-".repeat((n - starsCount) / 2)
-//   );
-// }
-// for (let i = 0; i < Math.floor(n / 2); i++) {
-// Draw the house body: |*****| }
-// }
+ for (let i = 0; i < Math.floor((n + 1) / 2); i++) {
+   if (n % 2 == 0) starsCount = 2 * (i + 1);
+   else starsCount = 2 * i + 1;
+   console.log(
+     "-".repeat((n - starsCount) / 2) +
+       "*".repeat(starsCount) +
+       "-".repeat((n - starsCount) / 2)
+   );
+ }
+ for (let i = 0; i < Math.floor(n / 2); i++) {
+ Draw the house body: |*****| }
+ }
