6e79494f23149038ce31dc193418761edf91097c
SilviyaIvanova <silviya.yordanova91@gmail.com>
2022-10-13 16:15:51 +0300

    Update theatrePromotions.js

--- 03. JavaScript Fundamentals/01 Basic Syntax. Conditional Statements and Loops/theatrePromotions.js
+++ 03. JavaScript Fundamentals/01 Basic Syntax. Conditional Statements and Loops/theatrePromotions.js
@@ -34,34 +34,34 @@ theatrePromotions("Weekday", 42);
 theatrePromotions("Holiday", -12);
 theatrePromotions("Holiday", 15);
 
-// function theatrePromotions(day, age) {
-//   if (age >= 0 && age <= 122) {
-//     switch (day) {
-//       case "Weekday":
-//         if ((0 <= age && age <= 18) || (64 < age && age <= 122)) {
-//           console.log(`12$`);
-//         } else if (18 < age && age <= 64) {
-//           console.log(`18$`);
-//         }
-//         break;
-//       case "Weekend":
-//         if ((0 <= age && age <= 18) || (64 < age && age <= 122)) {
-//           console.log(`15$`);
-//         } else if (18 < age && age <= 64) {
-//           console.log(`20$`);
-//         }
-//         break;
-//       case "Holiday":
-//         if (0 <= age && age <= 18) {
-//           console.log(`5$`);
-//         } else if (18 < age && age <= 64) {
-//           console.log(`12$`);
-//         } else if (64 < age && age <= 122) {
-//           console.log(`10$`);
-//         }
-//         break;
-//     }
-//   } else {
-//     console.log(`Error!`);
-//   }
-// }
+ function theatrePromotions(day, age) {
+   if (age >= 0 && age <= 122) {
+     switch (day) {
+       case "Weekday":
+         if ((0 <= age && age <= 18) || (64 < age && age <= 122)) {
+           console.log(`12$`);
+         } else if (18 < age && age <= 64) {
+           console.log(`18$`);
+         }
+         break;
+       case "Weekend":
+         if ((0 <= age && age <= 18) || (64 < age && age <= 122)) {
+           console.log(`15$`);
+         } else if (18 < age && age <= 64) {
+           console.log(`20$`);
+         }
+         break;
+       case "Holiday":
+         if (0 <= age && age <= 18) {
+           console.log(`5$`);
+         } else if (18 < age && age <= 64) {
+           console.log(`12$`);
+         } else if (64 < age && age <= 122) {
+           console.log(`10$`);
+         }
+         break;
+     }
+   } else {
+     console.log(`Error!`);
+   }
+ }
