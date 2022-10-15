0c260354669d8eb38a65c31dfc4264405da586b0
SilviyaIvanova <silviya.yordanova91@gmail.com>
2022-10-15 10:25:43 +0300

    Update 04.integerandFloat.js

--- 03. JavaScript Fundamentals/03 Data Types and Variables/04.integerandFloat.js
+++ 03. JavaScript Fundamentals/03 Data Types and Variables/04.integerandFloat.js
@@ -12,48 +12,48 @@ integerandFloat(9, 100, 1.1);
 integerandFloat(100, 200, 303);
 
 //--------------------------------------------------
-// function integerandFloat(a, b, c) {
-//     let sum = a + b + c;
-//     let type = "Integer";
-//     let roundedSum = Math.round(sum);
+ function integerandFloat(a, b, c) {
+     let sum = a + b + c;
+     let type = "Integer";
+     let roundedSum = Math.round(sum);
 
-//     if (sum != roundedSum) {
-//       type = "Float";
-//     }
-//     console.log(`${sum} - ${type}`);
-//   }
+     if (sum != roundedSum) {
+       type = "Float";
+     }
+     console.log(`${sum} - ${type}`);
+   }
 
 //--------------------------------------------------
-// function integerandFloat(a, b, c) {
-//     let sum = a + b + c;
-//     let sumAsString = String(sum);
-//     let type = "Integer";
+ function integerandFloat(a, b, c) {
+     let sum = a + b + c;
+     let sumAsString = String(sum);
+     let type = "Integer";
 
-//     for (let i = 0; i < sumAsString.length; i++) {
-//       if (sumAsString[i] == ".") {
-//         type = "Float";
-//       }
-//     }
-//     console.log(`${sum} - ${type}`);
-//   }
+     for (let i = 0; i < sumAsString.length; i++) {
+       if (sumAsString[i] == ".") {
+         type = "Float";
+       }
+     }
+     console.log(`${sum} - ${type}`);
+   }
 
 //-------------------------------------
-// function integerandFloat(a, b, c) {
-//   let sum = a + b + c;
-//   let resToStr = String(sum);
-//   let isFloat = false;
+ function integerandFloat(a, b, c) {
+   let sum = a + b + c;
+   let resToStr = String(sum);
+   let isFloat = false;
 
-//   for (let i = 0; i < resToStr.length; i++) {
-//     if (sumAsString[i] == ".") {
-//       isFloat = true;
-//     }
-//   }
-//   console.log(`${sum} - ${isFloat ? "Float" : "Integer"}`);
-// }
+   for (let i = 0; i < resToStr.length; i++) {
+     if (sumAsString[i] == ".") {
+       isFloat = true;
+     }
+   }
+   console.log(`${sum} - ${isFloat ? "Float" : "Integer"}`);
+ }
 
 //------------------------------------
-// function integerandFloat(a, b, c) {
-//   let sum = a + b + c;
+ function integerandFloat(a, b, c) {
+   let sum = a + b + c;
 
-//   console.log(`${sum} - ${parseInt(sum) == sum ? "Integer" : "Float"}`);
-// }
+   console.log(`${sum} - ${parseInt(sum) == sum ? "Integer" : "Float"}`);
+ }
