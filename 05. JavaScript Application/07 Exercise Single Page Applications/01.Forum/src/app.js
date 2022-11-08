16e65a07cf59354b5d3a8407ce147dc5c216f7f9
SilviyaIvanova91 <99129696+SilviyaIvanova91@users.noreply.github.com>
2022-11-08 18:13:45 +0200

    added exercises

---
+++ 05. JavaScript Application/07 Exercise Single Page Applications/01.Forum/src/app.js
@@ -0,0 +1,13 @@
+import { showHome } from "./home.js";
+
+document.querySelector("nav a").addEventListener("click", showHome);
+
+showHome();
+
+//------------------------------
+
+// import {showHome} from './home.js';
+
+// document.querySelector("nav a").addEventListener("click", showHome)
+
+// showHome();
