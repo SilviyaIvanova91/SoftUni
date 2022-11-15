256c1eaa6e9588ed5b07bb69580392874b18451b
SilviyaIvanova <silviya.yordanova91@gmail.com>
2022-11-15 12:54:13 +0200

    Update app.js

--- 05. JavaScript Application/11 Exercise Client Side Rendering/01.List-Towns/app.js
+++ 05. JavaScript Application/11 Exercise Client Side Rendering/01.List-Towns/app.js
@@ -14,3 +14,32 @@ document.getElementById("btnLoadTowns").addEventListener("click", (e) => {
   let root = document.getElementById("root");
   render(cardTemplate, root);
 });
+
+//------------Second condition---------------
+let form = document.getElementById("form");
+form.addEventListener("click", onSubmit);
+let root = document.getElementById("root");
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  let formData = new FormData(form);
+  const { towns } = Object.fromEntries(formData);
+  let townsArr = towns.split(", ");
+  renderTownList(townsArr);
+  form.reset();
+}
+
+function renderTownList(data) {
+  const result = createTownList(data);
+  render(result, root);
+}
+
+function createTownList(data) {
+  let ul = html`
+    <ul>
+      ${data.map((t) => html`<li>${t}</li>`)}
+    </ul>
+  `;
+  return ul;
+}
