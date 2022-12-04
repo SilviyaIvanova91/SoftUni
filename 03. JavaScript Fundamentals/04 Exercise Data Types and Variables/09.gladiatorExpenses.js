4353960791bfc16b1534518739ac5a8d0458cff1
SilviyaIvanova <silviya.yordanova91@gmail.com>
2022-12-04 11:49:50 +0200

    Update 09.gladiatorExpenses.js

--- 03. JavaScript Fundamentals/04 Exercise Data Types and Variables/09.gladiatorExpenses.js
+++ 03. JavaScript Fundamentals/04 Exercise Data Types and Variables/09.gladiatorExpenses.js
@@ -36,28 +36,27 @@ gladiatorExpenses(7, 2, 3, 4, 5);
 gladiatorExpenses(23, 12.5, 21.5, 40, 200);
 
 //----------------------------------------------
-// function gladiatorExpenses(
-//     lostFight,
-//     helmetPrice,
-//     swordPrice,
-//     shieldPrice,
-//     armourPrice
-//   ) {
-//    let all = 0
-
-//     for (let i = 1; i <= lostFight; i++) {
-//       if (i % 2 === 0) {
-//         all += helmetPrice;
-//       }
-//       if (i % 3 === 0) {
-//         all += swordPrice;
-//       }
-//       if (i % 6 === 0) {
-//         all += shieldPrice;
-//       }
-//       if (i % 12 === 0) {
-//         all +=armourPrice;
-//       }
-//     }
-//     console.log(`Gladiator expenses: ${all.toFixed(2)} aureus`);
-//   }
+ function gladiatorExpenses(
+     lostFight,
+     helmetPrice,
+     swordPrice,
+     shieldPrice,
+     armourPrice
+   ) {
+    let all = 0
+      for (let i = 1; i <= lostFight; i++) {
+       if (i % 2 === 0) {
+         all += helmetPrice;
+       }
+       if (i % 3 === 0) {
+         all += swordPrice;
+       }
+       if (i % 6 === 0) {
+         all += shieldPrice;
+       }
+       if (i % 12 === 0) {
+         all +=armourPrice;
+       }
+     }
+     console.log(`Gladiator expenses: ${all.toFixed(2)} aureus`);
+   }
