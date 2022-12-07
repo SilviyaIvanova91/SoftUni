5907e2f6ac423e3ba245c06737fc58ae999232a0
SilviyaIvanova <silviya.yordanova91@gmail.com>
2022-12-07 13:39:51 +0200

    Update 04.reverseinPlace.js

--- 03. JavaScript Fundamentals/05 Arrays/04.reverseinPlace.js
+++ 03. JavaScript Fundamentals/05 Arrays/04.reverseinPlace.js
@@ -8,49 +8,49 @@ function reverseinPlace(arr) {
   console.log(arr.join(" "));
 }
 
-// function reverseinPlace(arr) {
-//   for (let i = 0; i < arr.length / 2; i++) {
-//     let k = arr.length - 1 - i;
-//     let temp = arr[i];
-//     arr[i] = arr[k];
-//     arr[k] = temp;
-//   }
-//   console.log(arr.join(" "));
-// }
+ function reverseinPlace(arr) {
+   for (let i = 0; i < arr.length / 2; i++) {
+     let k = arr.length - 1 - i;
+     let temp = arr[i];
+     arr[i] = arr[k];
+     arr[k] = temp;
+   }
+   console.log(arr.join(" "));
+ }
 
 //------------------------------------------А
 //repeat for all elements from begining to middle
 // - swar current element (start+i) with element in other half (end-1)
 //- use temp variable to store value during swap
-//
-// [10 , 20, 30, 40, 50, 60, 70] // 7 ->3.5
-// [10 , 20, 30, 40, 50, 60] // 6 ->3
-//
-//   0    1   2   3   4   5   6
-//*
-//0....length
-// i < length / 2 = 3.5
-//
-//
-//0->6
-//1->5
-//2->4
-//3->3
-//
-// let k = length -1-i
+
+ [10 , 20, 30, 40, 50, 60, 70] // 7 ->3.5
+ [10 , 20, 30, 40, 50, 60] // 6 ->3
+
+   0    1   2   3   4   5   6
+*
+0....length
+ i < length / 2 = 3.5
+
+
+0->6
+1->5
+2->4
+3->3
+
+ let k = length -1-i
 
 reverseinPlace(["a", "b", "c", "d", "e"]);
 reverseinPlace(["abc", "def", "hig", "klm", "nop"]);
 reverseinPlace(["33", "123", "0", "dd"]);
 
 //----------------------------------------------------
-// function reverseinPlace(arr) {
-//   let k = arr.length - 1;
-//   for (let i = 0; i < arr.length / 2; i++, k--) {
-//     let k = arr.length - 1 - i;
-//     let temp = arr[i];
-//     arr[i] = arr[k];
-//     arr[k] = temp;
-//   }
-//   console.log(arr.join(" "));
-// }
+ function reverseinPlace(arr) {
+   let k = arr.length - 1;
+   for (let i = 0; i < arr.length / 2; i++, k--) {
+     let k = arr.length - 1 - i;
+     let temp = arr[i];
+     arr[i] = arr[k];
+     arr[k] = temp;
+   }
+   console.log(arr.join(" "));
+ }
