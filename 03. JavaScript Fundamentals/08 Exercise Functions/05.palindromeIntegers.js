8f3a54613c32788882d3f86ed7fc9c4859c796fb
SilviyaIvanova <silviya.yordanova91@gmail.com>
2023-01-04 12:09:44 +0200

    Update 05.palindromeIntegers.js

--- 03. JavaScript Fundamentals/08 Exercise Functions/05.palindromeIntegers.js
+++ 03. JavaScript Fundamentals/08 Exercise Functions/05.palindromeIntegers.js
@@ -14,20 +14,20 @@ function palindromeIntegers(inputArr) {
 palindromeIntegers([123, 323, 421, 121]);
 palindromeIntegers([32, 2, 232, 1010]);
 
-// function palindromeIntegers(inputArr) {
-//   let arrOfNums = inputArr;
+ function palindromeIntegers(inputArr) {
+   let arrOfNums = inputArr;
 
-//   function isPalindrom(number) {
-//     let startNum = number;
-//     let reversedNum = Number(number.toString().split("").reverse().join(""));
-//     if (startNum === reversedNum) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   }
-//   for (let i = 0; i < arrOfNums.length; i++) {
-//     let currNum = arrOfNums[i];
-//     console.log(isPalindrom(currNum));
-//   }
-// }
+   function isPalindrom(number) {
+     let startNum = number;
+     let reversedNum = Number(number.toString().split("").reverse().join(""));
+     if (startNum === reversedNum) {
+       return true;
+     } else {
+       return false;
+     }
+   }
+   for (let i = 0; i < arrOfNums.length; i++) {
+     let currNum = arrOfNums[i];
+     console.log(isPalindrom(currNum));
+   }
+ }
