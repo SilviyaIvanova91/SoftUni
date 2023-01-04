258ecdf2ea6234a41c8c405f70a8f5a55c166751
SilviyaIvanova <silviya.yordanova91@gmail.com>
2023-01-04 12:06:52 +0200

    Update 01.smallestofThreeNumbers.js

--- 03. JavaScript Fundamentals/08 Exercise Functions/01.smallestofThreeNumbers.js
+++ 03. JavaScript Fundamentals/08 Exercise Functions/01.smallestofThreeNumbers.js
@@ -15,33 +15,33 @@ smallestofThreeNumbers(600, 342, 123);
 smallestofThreeNumbers(25, 21, 4);
 smallestofThreeNumbers(2, 2, 2);
 
-// function smallestofThreeNumbers(numOne, numTwo, numThree) {
-//   let smallestNum = Math.min(numOne, numTwo, numThree);
-//   console.log(smallestNum);
-// }
+ function smallestofThreeNumbers(numOne, numTwo, numThree) {
+   let smallestNum = Math.min(numOne, numTwo, numThree);
+   console.log(smallestNum);
+ }
 //-------------------------------------------------------
-// function smallestofThreeNumbers(numOne, numTwo, numThree) {
-//   let smallestNum = function (numOne, numTwo, numThree) {
-//     if (numOne <= numTwo && numOne < numThree) {
-//       smallestNum = numOne;
-//     } else if (numTwo <= numOne && numTwo <= numThree) {
-//       smallestNum = numTwo;
-//     } else {
-//       smallestNum = numThree;
-//     }
-//     return smallestNum;
-//   };
-//   console.log(smallestNum(numOne, numTwo, numThree));
-// }
+ function smallestofThreeNumbers(numOne, numTwo, numThree) {
+   let smallestNum = function (numOne, numTwo, numThree) {
+     if (numOne <= numTwo && numOne < numThree) {
+       smallestNum = numOne;
+     } else if (numTwo <= numOne && numTwo <= numThree) {
+       smallestNum = numTwo;
+     } else {
+       smallestNum = numThree;
+     }
+     return smallestNum;
+   };
+   console.log(smallestNum(numOne, numTwo, numThree));
+ }
 
-// smallestofThreeNumbers(2, 5, 3);
-// smallestofThreeNumbers(600, 342, 123);
-// smallestofThreeNumbers(25, 21, 4);
-// smallestofThreeNumbers(2, 2, 2);
+ smallestofThreeNumbers(2, 5, 3);
+ smallestofThreeNumbers(600, 342, 123);
+ smallestofThreeNumbers(25, 21, 4);
+ smallestofThreeNumbers(2, 2, 2);
 
 //------------------------------------------------------------
-// function smallestofThreeNumbers(numOne, numTwo, numThree) {
-//   let smallestNum = (numOne, numTwo, numThree) =>
-//     Math.min(numOne, numTwo, numThree);
-//   console.log(smallestNum(numOne, numTwo, numThree));
-// }
+ function smallestofThreeNumbers(numOne, numTwo, numThree) {
+   let smallestNum = (numOne, numTwo, numThree) =>
+     Math.min(numOne, numTwo, numThree);
+   console.log(smallestNum(numOne, numTwo, numThree));
+ }
