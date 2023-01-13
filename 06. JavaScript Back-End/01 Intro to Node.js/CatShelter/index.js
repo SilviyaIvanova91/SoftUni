3566bcc8af626f6dbd00e5b876d604c7c320ac88
SilviyaIvanova91 <99129696+SilviyaIvanova91@users.noreply.github.com>
2023-01-13 11:30:51 +0200

    added Cat Shelter from Intro to Node.js Lab

---
+++ 06. JavaScript Back-End/01 Intro to Node.js/CatShelter/index.js
@@ -0,0 +1,70 @@
+const fs = require("fs/promises");
+const fss = require("fs");
+
+const http = require("http");
+const qs = require("querystring");
+
+const cats = require("./cats.json");
+
+// const homePage = require("./views_old");
+// const editCat = require("./views_old/editCat");
+// const siteCss = require("./css/site.css");
+
+const server = http.createServer(async (req, res) => {
+  console.log(req.url);
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+  });
+
+  if (req.url == "/") {
+    const homePage = await readFile("./views/home.html");
+    const catsHtml = cats.map((cat) => catTemplate(cat)).join("");
+    let result = homePage.replace("{{cats}}", catsHtml);
+
+    res.write(result);
+  } else if (/cats\/\d+\/edit/.test(req.url)) {
+    //let catId = req.url.split("/")[2];
+    //let cat = cats.find((x) => x.id == catId);
+    let editCatHtml = await readFile("./views/editCat.html");
+    let qsData = qs.parse(req.url.split("?")[1]);
+    console.log(qsData);
+
+    res.write(editCatHtml);
+  } else if (req.url == "/css/site.css") {
+    res.writeHead(200, {
+      "Content-Type": "text/css",
+    });
+
+    const siteCss = await readFile("./content/styles/site.css");
+    res.write(siteCss);
+  } else {
+    res.write(`<h1>404</h1>`);
+  }
+
+  res.end();
+});
+
+function readFile(path) {
+  return fs.readFile(path, { encoding: "utf-8" });
+}
+
+function catTemplate(cat) {
+  const html = fss.readFileSync("./views/partials/cat.html", {
+    encoding: "utf-8",
+  });
+
+  // let result = html.replaceAll("{{name}}", cat.name);
+  // result = result.replace("{{description}}", cat.description);
+  // result = result.replace("{{imageUrl}}", cat.imageUrl);
+  // result = result.replace("{{breed}}", cat.breed);
+  // result = result.replace("{{id}}", cat.id);
+
+  const modufyHtml = Object.keys(cat).reduce((result, key) => {
+    return result.replaceAll(`{{${key}}}`, cat[key]);
+  }, html);
+
+  return modufyHtml;
+}
+
+server.listen(3000);
+console.log("Server is running on port 3000...");
