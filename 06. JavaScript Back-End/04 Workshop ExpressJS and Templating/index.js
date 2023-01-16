ca11f3ef4fe2626d4b426e5a71a0316695487096
SilviyaIvanova <silviya.yordanova91@gmail.com>
2023-01-16 15:42:31 +0200

    Update index.js

--- 06. JavaScript Back-End/04 Workshop ExpressJS and Templating/index.js
+++ 06. JavaScript Back-End/04 Workshop ExpressJS and Templating/index.js
@@ -27,18 +27,4 @@ app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
 
-// const databaseConfig = require("./config/database");
-// const expressConfig = require("./config/express");
-// const routesConfig = require("./config/routes");
 
-// async function start() {
-//   const app = express();
-
-//   await databaseConfig(app);
-//   expressConfig(app);
-//   routesConfig(app);
-
-//   app.listen(3000, () => console.log("Server listening on port 3000"));
-// }
-
-//start();
