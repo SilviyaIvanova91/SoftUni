cf146f73c78de5bda6f78d813f005854190b2473
SilviyaIvanova91 <99129696+SilviyaIvanova91@users.noreply.github.com>
2023-01-17 13:31:47 +0200

    +

--- 06. JavaScript Back-End/04 Workshop ExpressJS and Templating/controllers/defaultController.js
+++
@@ -1,3 +0,0 @@
-module.exports = (req, res) => {
-  res.status(404).render("404");
-};
--- 06. JavaScript Back-End/04 Workshop ExpressJS and Templating/middlewares/defaultTitle.js
+++
@@ -1,4 +0,0 @@
-module.exports = (defaultTitle) => (req, res, next) => {
-  res.locals.title = defaultTitle;
-  next();
-};
