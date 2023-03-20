ec284adcb9933672995853f77f8aaf2c5fd30073
SilviyaIvanova91 <99129696+SilviyaIvanova91@users.noreply.github.com>
2023-03-20 23:18:00 +0200

    add demo

---
+++ 07. React JS/11. Workshop Advanced Techniques - Part 1/11. Workshop Advanced Techniques - Part 1/class-components/src/components/TaskItem.js
@@ -0,0 +1,31 @@
+import { Component } from "react";
+import styles from './TaskItem.module.css';
+
+import { TaskContext } from '../contexts/TaskContext';
+
+class TaskItem extends Component {
+    render() {
+        const className = this.props.isCompleted ? styles.completed : '';
+
+        return (
+            <TaskContext.Consumer>
+                {context =>
+                    <>
+                        <li
+                            onClick={() => this.props.onClick(this.props.title)}
+                            className={className}
+                        >
+                            {this.props.title}
+                            <button onClick={(e) => context.taskDeleteHandler(e, this.props.title)}>
+                                x
+                            </button>
+                        </li>
+                     
+                    </>
+                }
+            </TaskContext.Consumer>
+        );
+    }
+}
+
+export default TaskItem;
---
+++ 07. React JS/11. Workshop Advanced Techniques - Part 1/11. Workshop Advanced Techniques - Part 1/class-components/src/components/TaskList.js
@@ -0,0 +1,96 @@
+import { Component } from "react";
+import TaskItem from './TaskItem';
+import withRouter from '../hoc/withRouter';
+import { TaskContext } from "../contexts/TaskContext";
+
+class TaskList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            tasks: [
+                { title: 'Task 11', isCompleted: false },
+                { title: 'Task 22', isCompleted: false },
+                { title: 'Task 33', isCompleted: false },
+            ],
+            filter: 'all',
+            newTask: '',
+            character: {},
+        };
+
+        this.newTaskChangeHandler = this.newTaskChangeHandler.bind(this);
+    }
+
+    componentDidMount() {
+        fetch(`https://swapi.dev/api/people/4`)
+            .then(res => res.json())
+            .then(result => {
+                this.setState({ character: result });
+            });
+    }
+
+    componentDidUpdate() {
+        // console.log('Did update');
+    }
+
+    newTaskChangeHandler(e) {
+        this.setState({ newTask: e.target.value });
+    }
+
+    addNewTaskHandler(e) {
+        e.preventDefault();
+
+        this.setState((state) => ({
+            tasks: [...state.tasks, { title: state.newTask, isCompleted: false }],
+            newTask: '',
+        }));
+    }
+
+    taskClickHandler(taskTitle) {
+        this.setState(state => ({
+            tasks: state.tasks.map(x => x.title === taskTitle ? { ...x, isCompleted: !x.isCompleted } : x)
+        }));
+    }
+
+    taskDeleteHandler(e, taskTitle) {
+        e.stopPropagation();
+
+        this.setState(state => ({
+            tasks: state.tasks.filter(x => x.title !== taskTitle)
+        }))
+    }
+
+    render() {
+        return (
+            <TaskContext.Provider value={{ taskDeleteHandler: this.taskDeleteHandler.bind(this) }}>
+                <h2>Current Character: {this.state.character.name}</h2>
+
+                <ul>
+                    {this.state.tasks.map(x =>
+                        <TaskItem
+                            key={x.title}
+                            title={x.title}
+                            isCompleted={x.isCompleted}
+                            onClick={this.taskClickHandler.bind(this)}
+                        />
+                    )}
+                </ul>
+
+                <form onSubmit={this.addNewTaskHandler.bind(this)}>
+                    <label htmlFor="new-task"></label>
+                    <input
+                        type="text"
+                        id="new-task"
+                        name="newTask"
+                        value={this.state.newTask}
+                        onChange={this.newTaskChangeHandler}
+                    />
+
+                    <input type="submit" value="Add" />
+                </form>
+            </TaskContext.Provider>
+        );
+    }
+}
+
+export default withRouter(TaskList);
